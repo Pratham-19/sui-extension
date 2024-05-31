99cce73ca98fdb545e37e984a4c63806c642d719
pratham-19 <prathamsinghal2003@gmail.com>
2024-05-31 16:04:00 +0530

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+import { commands, ExtensionContext, window } from "vscode";
+import { HelloWorldPanel } from "./panels/HelloWorldPanel";
+
+export function activate(context: ExtensionContext) {
+  // Create the show hello world command
+  const showHelloWorldCommand = commands.registerCommand(
+    "hello-world.showHelloWorld",
+    () => {
+      HelloWorldPanel.render(context.extensionUri);
+    }
+  );
+
+  // Add command to the extension context
+  context.subscriptions.push(showHelloWorldCommand);
+
+  // let view = window.registerWebviewViewProvider(
+
+  // );
+}
